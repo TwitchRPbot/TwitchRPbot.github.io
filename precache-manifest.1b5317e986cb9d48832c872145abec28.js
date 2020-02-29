4536695b5c59959bc7da188d6f2c0860080acb84
Dowen <tokenyete@gmail.com>
2020-02-29 15:14:04 +0800

    Updates

---
+++ precache-manifest.1b5317e986cb9d48832c872145abec28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45270c6244771643421e79aa9e1e9b95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db83e96ea81aae70d384",
+    "url": "/static/js/2.4abf2b36.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.4abf2b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093f0f856ca7e5bca02e",
+    "url": "/static/js/main.a771f418.chunk.js"
+  },
+  {
+    "revision": "f56df5be13d4be260ffd",
+    "url": "/static/js/runtime-main.29f8e171.js"
+  }
+]);
